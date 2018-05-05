a74919ee3a3144f1d3c9e1a786b5ff3abec9f485
Malcolm Jones <malcolm@adobe.com>
2018-05-05 12:13:53 -0400

    wip: makefile

--- .ci/flatpak-bootstrap.sh
+++ .ci/flatpak-bootstrap.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+
+# SOURCE: https://github.com/ghjnut/docker-steamcmd/blob/2f409cbd7e841b2f910c34c26df10f81fa9d408f/bin/build_auth
+[[ "$TRACE" ]] && set -x
+
 cd ..
 make bootstrap-runtime-user
