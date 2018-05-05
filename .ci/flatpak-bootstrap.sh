2b4e3de08bdccebf62ec33e5f446b3fa1220ca3e
Malcolm Jones <malcolm@adobe.com>
2018-05-05 13:25:34 -0400

    wip: makefile

--- .ci/flatpak-bootstrap.sh
+++ .ci/flatpak-bootstrap.sh
@@ -5,9 +5,11 @@
 
 # grab sudo priviledges so we do not need to interrupt the following process if they are needed
 # SOURCE: https://github.com/niccokunzmann/cp-docker-development/blob/212363005e107525bdbb9780508ec0d1b11b0f6c/docker/ubuntu/build/run_all.sh
-sudo true
+# sudo true
 
 # ---------------------------------------------
 # Install Flatpak Remotes And Runtimes
 # ---------------------------------------------
-make bootstrap-runtime-user
+# make bootstrap-runtime-user
+
+/home/developer/.ci/bootstrap-runtime-user
