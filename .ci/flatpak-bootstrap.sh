f0f98a36898192ea9a915a7519f518da7e2c11cc
Malcolm Jones <malcolm@adobe.com>
2018-05-05 13:04:26 -0400

    wip: makefile

--- .ci/flatpak-bootstrap.sh
+++ .ci/flatpak-bootstrap.sh
@@ -3,7 +3,11 @@
 # SOURCE: https://github.com/ghjnut/docker-steamcmd/blob/2f409cbd7e841b2f910c34c26df10f81fa9d408f/bin/build_auth
 [[ "$TRACE" ]] && set -x
 
+echo `pwd`
+
 cd ..
+
+echo `pwd`
 
 # ---------------------------------------------
 # Install Flatpak Remotes And Runtimes
