b4e0df29b6843bc675ba4abd1eead0032053656e
Malcolm Jones <malcolm@adobe.com>
2018-05-06 23:20:16 -0400

    wip: makefile

--- data/install-desktop-file.sh
+++ data/install-desktop-file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-desktop-file-edit --set-key Name --set-value 'ScarlettOS' --set-key Exec --set-value 'who' --set-key Icon --set-value org.scarlett.ScarlettOS debian/ScarlettOS.desktop
+desktop-file-edit --set-key Name --set-value 'ScarlettOS' --set-key Exec --set-value 'who' --set-key Icon --set-value org.scarlett.ScarlettOS ScarlettOS.desktop
 
-install -D debian/ScarlettOS.desktop /app/share/applications/org.scarlett.ScarlettOS.desktop
+install -D ScarlettOS.desktop /app/share/applications/org.scarlett.ScarlettOS.desktop
